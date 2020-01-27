66927686cd956fcaf2d18f4624c1ad10863abc04
Joseba <joseba.merino.pina@gmail.com>
2020-01-27 13:13:50 +0100

    Ulitoms cambios

--- js/main.js
+++ js/main.js
@@ -2,15 +2,57 @@ console.log("Hola index");
 
 let inputEL = document.getElementById('idProducto');
 let botonEL = document.getElementById('boton');
-let resultadoEL = document.getElementById('resultado');
 
-botonEL.addEventListener("click", () => {
+let imagenEL = document.getElementById('imagen');
+let nombreEL = document.getElementById('nombre');
+let descripcionEL = document.getElementById('descripcion');
+
+let productoEL = document.getElementById('producto');
+
+let listaProductosEL = document.getElementById('lista-productos');
+
+
+
+window.onload = () => {
+    // Ejecuta las cosas cuando el documento este cargado
+    console.log('DOM cargado correctamente');
+    cargarDatos();
+}
+
+function cargarDatos() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4) {
+            if(xhr.status === 200) {
+                let productos = JSON.parse(xhr.responseText);
+                for(let producto of productos) {
+                    let liEL = document.createElement('li');
+                    liEL.className = 'list-group-item spinner';
+
+                    let nombreEL = document.createTextNode(producto.nombre);
+                    liEL.appendChild(nombreEL);
+
+                    liEL.addEventListener("click", () => {
+                        cargarProducto(producto.id);
+                    });
+
+                    listaProductosEL.appendChild(liEL);
+                }
+            } else {
+                listaProductosEL.innerHTML = xhr.status;
+            }
+        }
+    }
+    xhr.open('Get', `http://localhost:8080/supermerkado-rest/producto/`)
+    xhr.send(); // CUIDADO ES ASINCRONO!!
+}
+
+
+function cargarProducto(id) {
     console.debug('click boton');
 
-    let input = inputEL.value;
-
-    if(input === "") {
-        resultadoEL.innerHTML = "Escribe algo por favor";
+    if(id === "") {
 
     } else {
         // Llamada AJAX
@@ -22,18 +64,28 @@ botonEL.addEventListener("click", () => {
             //console.debug("responseText: " + xhr.responseText)
 
             if(xhr.readyState === 4) {
-                let producto = JSON.parse(xhr.responseText);
-                console.log("producto: " + producto);
-                resultadoEL.innerHTML = producto;
+                if(xhr.status === 200){
+                    let producto = JSON.parse(xhr.responseText);
+                    console.log("producto: " + producto);
+                    imagenEL.src = producto.imagen;
+                    imagenEL.className = 'imagen';
+                    nombreEL.innerHTML = producto.nombre;
+                    descripcionEL.innerHTML = producto.descripcion;
+                    productoEL.className = productoEL.className.replace( /(?:^|\s)d-none(?!\S)/g , '' )
+                } else if(xhr.status === 404) {
+
+                }
             }
         }
 
-        xhr.open('Get', `http://localhost:8080/supermerkado-rest/producto/${input}`)
+        xhr.open('Get', `http://localhost:8080/supermerkado-rest/producto/${id}`)
         xhr.send(); // CUIDADO ES ASINCRONO!!
-
-        
     }
 
 
+}
 
-})
+botonEL.addEventListener("click", () => {
+    let input = inputEL.value;
+    cargarProducto(input);
+});
